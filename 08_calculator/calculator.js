dec18f0402a20b4511a5ce1d75b9e93a173153ad
Kha Tran <khadieptran@gmail.com>
2021-08-18 12:01:38 -0400

    08 calcultor power

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,15 +7,27 @@ const subtract = function() {
 };
 
 const sum = function() {
-	
+  let numbers = arguments[0];
+  return numbers.reduce(function(total, number) {
+    return total += number;
+  }, 0)
 };
 
 const multiply = function() {
-
+  let numbers = arguments[0];
+  return numbers.reduce(function(product, number) {
+    return product *= number;
+  }, 1)
 };
 
 const power = function() {
-	
+	let number = arguments[0];
+  let power = arguments[1];
+  let result = 1;
+  for (let i = 0; i < power; i++) {
+    result *= number;
+  }
+  return result;
 };
 
 const factorial = function() {
